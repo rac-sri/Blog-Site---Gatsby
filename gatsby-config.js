03cb0bc7c34816a1ae2662a843fa9f6148a37a49
rachit2501 <rac.sri25@gmail.com>
2020-06-07 07:25:34 +0530

    done

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,5 +35,20 @@ module.exports = {
         path: 'images',
       },
     },
+    {
+      resolve: 'gatsby-source-instagram',
+      options: {
+        username: 'gatsbyjs',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/layout.tsx';
 import usePosts from '../hooks/usepost';
 import Postreview from '../components/postPreview';
 import Hero from '../components/hero';
+import Insta from '../components/insta';
 
 export default () => {
   const posts = usePosts();
@@ -20,6 +21,7 @@ export default () => {
         {posts.map(post => (
           <Postreview key={post.slug} post={post} />
         ))}
+        <Insta />
       </Layout>
     </>
   );
